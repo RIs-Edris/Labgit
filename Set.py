eb4c8071d96e20f204e31116074a202878ba528b
RIsss <edris.s@rmutsvmail.com>
2021-12-29 01:33:11 +0700

    add new sourcecode and add .gitignore

---
+++ Set.py
@@ -0,0 +1,23 @@
+dataset = 45,25,48,75,62,82,91,26,52,96,40,60,61,82,72,60
+a = 0
+b = 0
+c = 0
+d = 0
+f = 0
+
+for x in dataset:
+    if x >= 80:
+        a=a+1
+    elif x >= 70:
+        b=b+1
+    elif x >= 60:
+        c=c+1
+    elif x >= 50:
+        d=d+1
+    else:
+        f=f+1
+print("Number of Student have grade A = " + str(a))
+print("Number of Student have grade B = " + str(b))
+print("Number of Student have grade C = " + str(c))
+print("Number of Student have grade D = " + str(d))
+print("Number of Student have grade F = " + str(f))
